4cbea4bd67b8bd321c388924eb0d3347476f1339
willneedit <willneedit@nurfuerspam.de>
2025-01-13 10:19:05 +0100

    Added Prebuild task (e.g. for packing the Extras archives)

---
+++ Source/Sceelix.Designer.PreBuildProject/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Compression;
+using System.IO;
+
+namespace Sceelix.Designer.PreBuildProject
+{
+    internal class Program
+    {
+        static void PackToZip(string dir)
+        {
+            string targetZip = $"{dir}.zip";
+
+            if(File.Exists(targetZip)) File.Delete(targetZip);
+            ZipFile.CreateFromDirectory(dir, targetZip);
+
+        }
+
+        static int Main(string[] args)
+        {
+            if (args.Length < 1) return 100;
+
+            PackToZip($"{args[0]}/../Extras/API");
+            PackToZip($"{args[0]}/../Extras/Samples");
+            PackToZip($"{args[0]}/../Extras/Unity Plugin");
+
+            return 0;
+        }
+    }
+}
